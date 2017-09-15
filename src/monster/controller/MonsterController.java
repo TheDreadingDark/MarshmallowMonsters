2f267e042c9c9e87893cd4313b71f6db6d29729e
TheDreadingDark <31518923+TheDreadingDark@users.noreply.github.com>
2017-09-15 11:46:55 -0600

    Created controller framework.

---
+++ src/monster/controller/MonsterController.java
@@ -0,0 +1,11 @@
+package monster.controller;
+
+public class MonsterController
+{
+
+	public static void main(String [ ] args)
+	{
+		MonsterController app = new MonsterController();
+		app.start();
+	}
+}
