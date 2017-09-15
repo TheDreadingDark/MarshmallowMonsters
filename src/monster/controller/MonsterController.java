f99c1b7723b7b4c5430583172ee663086dce25fa
TheDreadingDark <31518923+TheDreadingDark@users.noreply.github.com>
2017-09-15 13:30:16 -0600

    Added getters and setters to MarshmallowMonster class.

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,9 +1,34 @@
 package monster.controller;
 
+import monster.model.MarshmallowMonster;
+import java.util.Scanner;
+
 public class MonsterController
 {
+	
 	public void start()
 	{
+		MarshmallowMonster basic = new MarshmallowMonster();
+		System.out.println(basic);
+		MarshmallowMonster galactus = new MarshmallowMonster("Scary Galactus Destroyer of Worlds Monster", 7, 3, 6.7, true);
+		System.out.println(galactus);
+		System.out.println("I am feeling hungry, I am going to eat one of Galactus' arms");
+		galactus.setArmCount(galactus.getArmCount() - 1);
+		System.out.println(galactus);
+		galactus.setArmCount(galactus.getArmCount() - 1);
+		System.out.println(galactus);
+	
+		interactWithMonster(galactus);
+	}
+	
+	private void interactWithMonster(MarshmallowMonster currentMonster)
+	{
+		Scanner myScanner = new Scanner(System.in);
+		System.out.println(currentMonster.getName() + " wants to know how many eyes you want to eat, please type in how many");
+		int consumed = myScanner.nextInt();
+		currentMonster.setEyeCount(currentMonster.getEyeCount() - consumed);
+		System.out.println(currentMonster);
 		
+		myScanner.close();
 	}
 }
--- src/monster/model/MarshmallowMonster.java
+++ src/monster/model/MarshmallowMonster.java
@@ -15,12 +15,57 @@ public class MarshmallowMonster
 		//initializes everything to 0, false or null.
 	}
 	
-	public MarshmallowMonster(String name, int eyeCount, int arms, int tentacleCount, boolean hasBloop)
+	public MarshmallowMonster(String name, int eyeCount, int arms, double tentacleCount, boolean hasBloop)
 	{
 		this.name = name;
 		this.eyeCount = eyeCount;
 		this.armCount = arms;
 		this.tentacleAmount = tentacleCount;
+		this.hasBloop = hasBloop;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public int getEyeCount()
+	{
+		return eyeCount;
+	}
+	
+	public int getArmCount()
+	{
+		return armCount;
+	}
+	
+	public double getTentacleAmount()
+	{
+		return tentacleAmount;
+	}
+	
+	public boolean gethasBloop()
+	{
+		return hasBloop;
+	}
+	
+	public void setEyeCount(int eyeCount)
+	{
+		this.eyeCount = eyeCount;
+	}
+	
+	public void setArmCount(int armCount)
+	{
+		this.armCount = armCount;
+	}
+	
+	public void setTentacleAmount(double tentacleAmount)
+	{
+		this.tentacleAmount = tentacleAmount;
+	}
+	
+	public void setBloop(boolean hasBloop)
+	{
 		this.hasBloop = hasBloop;
 	}
 	
